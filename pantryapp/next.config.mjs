cb3ecef742e6c8a3c603dd12d778fea648f28111
D-G Singla <dsingla340@gmail.com>
2024-07-31 13:54:23 -0400

    Fixed

--- pantryapp/next.config.mjs
+++ pantryapp/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/pantryapp/app',  // Add base path if applicable
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/app', // Adjust as needed
+      },
+    ];
+  },
 };
 
 export default nextConfig;
