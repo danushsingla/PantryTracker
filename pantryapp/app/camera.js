5a85a195742046262acb8d090cb90f9a10508a0a
D-G Singla <dsingla340@gmail.com>
2024-08-01 00:14:41 -0400

    Added uploading to Firebase

--- pantryapp/app/camera.js
+++ pantryapp/app/camera.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from "react";
 import { Camera } from "react-camera-pro";
 import { Box, Button } from "@mui/material";
+import { storage } from "@/firebase";
+import { ref, uploadBytesResumable } from "firebase/storage";
 
 export const CameraComponent = () => {
   const camera = useRef(null);
@@ -13,9 +15,47 @@ export const CameraComponent = () => {
     setPictureTaken(true);
   }
 
+  // Because webcam images are base64 encoded, we need to convert it to a blob
+  const dataURLToBlob = (dataURL) => {
+    const [header, data] = dataURL.split(',');
+    const mime = header.split(':')[1].split(';')[0];
+    const binary = atob(data);
+    const array = [];
+    for (let i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], { type: mime });
+  };
+
   const photoConfirmed = () => {
-    console.log("Photo is confirmed.")
-  }
+    if(image) {
+        const blob = dataURLToBlob(image);
+        const storageRef = ref(storage, `images/${Date.now()}.jpg`);
+        const uploadTask = uploadBytesResumable(storageRef, blob);
+
+        // Progress bar
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+                }
+            },
+            (error) => {
+                console.error(error);
+            },
+            () => {
+                console.log('Upload complete');
+            }
+        );
+    }
+}
 
   const imgStyle = {
     width: '100%', // Adjust width as needed
--- pantryapp/firebase.js
+++ pantryapp/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 console.log(process.env)
 
@@ -17,5 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export {app, firestore}
+export {app, firestore, storage}
